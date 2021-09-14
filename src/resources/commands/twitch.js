41b16a9312e6346cc1d44356c7dc97e945a5a4be
Jason <pix3l.icious@gmail.com>
2021-09-14 09:54:31 +0700

    💜 twitch.json - Moved TwitchAPI properties from config.json to api.json

--- src/resources/commands/twitch.js
+++ src/resources/commands/twitch.js
@@ -5,19 +5,21 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 const config = require('../config.json');
+const api = require('../json/api.json');
+const socials = require('../json/socials.json');
 const fetch = require('node-fetch')
-const TwitchURL = `https://api.twitch.tv/helix/streams/${config.socials.links.twitch}`
+const TwitchURL = `${api.twitch.url}${socials.links.twitch}`
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('twitch')
-        .setDescription(`[IN BETA, DO NOT USE] Shows whether ${config.socials.name} is live or not!`),
+        .setDescription(`[IN BETA, DO NOT USE] Shows whether ${socials.name} is live or not!`),
     async execute(interaction) {
         fetch(TwitchURL, {
             method: 'GET',
             headers: {
-                "Client-ID": config.api.twitch.clientID,
-                "Authorization": 'Bearer ' + config.api.twitch.token
+                "Client-ID": api.twitch.clientID,
+                "Authorization": 'Bearer ' + api.twitch.token
             }
         }).then(res => res.json()).then(res => {
             console.log(res)
@@ -28,21 +30,21 @@ module.exports = {
             .addComponents(
                 new MessageButton()
                         .setStyle('LINK')
-                    .setLabel(`Visit ${config.socials.links.twitch}'s Twitch here!`)
+                    .setLabel(`Visit ${socials.links.twitch}'s Twitch here!`)
                     .setURL(`https://namemc.com/profile/${findPlayer.uuid}`)
             )
         
             const embed = new MessageEmbed()
                 .setColor(config.embed.colour)
-                .setAuthor(config.socials.links.twitch, config.socials.picture)
-                .setTitle(`Watch ${config.socials.links.twitch} on Twitch!`)
-                .setURL(`https://twitch.tv/${config.socials.links.twitch}`)
-                .setImage(config.socials.picture)
+                .setAuthor(socials.links.twitch, socials.picture)
+                .setTitle(`Watch ${socials.links.twitch} on Twitch!`)
+                .setURL(`https://twitch.tv/${socials.links.twitch}`)
+                .setImage(socials.picture)
         
             if (res.stream == null) {
-                interaction.reply({ content: `**${config.socials.links.twitch}** is not currently live, but you can still check out ${config.socials.pronouns.pronouns3} Twitch.`, components: [button] });
+                interaction.reply({ content: `**${socials.links.twitch}** is not currently live, but you can still check out ${socials.pronouns.pronouns3} Twitch.`, components: [button] });
             } else {
-                interaction.reply({ content: `**${config.socials.links.twitch}** is currently live!`, embeds: [embed], components: [button] });
+                interaction.reply({ content: `**${socials.links.twitch}** is currently live!`, embeds: [embed], components: [button] });
             }
         });
         
